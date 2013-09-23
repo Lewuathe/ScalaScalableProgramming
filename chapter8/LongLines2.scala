58415b3f23341482052e76d09be06608705e6ddc
Lewuathe <lewuathe@me.com>
2013-09-23 22:41:59 +0900

    FindLongLines

---
+++ chapter8/LongLines2.scala
@@ -0,0 +1,13 @@
+import scala.io.Source
+
+object LongLines {
+  def processFile(filename: String, width: Int) {
+    val source = Source.fromFile(filename)
+    def processLine(filename: String, width: Int, line: String) {
+      if (line.length > width) 
+        println(filename + ": " + line.trim)
+    }
+    for (line <- source.getLines()) 
+      processLine(filename, width, line)
+  }
+}
